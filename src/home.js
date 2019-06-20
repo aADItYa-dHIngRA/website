7026647157c73cdc6219445dc20e6c750e7a6f25
Raj-Bhavan <aadi.dhingra@gmail.com>
2019-06-20 17:18:44 -0400

    Create home.js

---
+++ src/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg'
+import './App.css'
+class Home extends React.Component{
+  render(){
+    return (
+
+    <div className='App'>
+    	<p className = 'App-header'>This is my web site</p>
+    </div>
+  
+  );
+  }
+  
+}
+
+export default Home;
